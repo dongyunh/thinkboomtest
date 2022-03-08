4abbc8114b70da40127a1b04d6a8336ca6552662
yechan <noy3928@naver.com>
2022-03-09 00:59:12 +0900

    feat : make step up feature

--- pages/randomWord/index.tsx
+++ pages/randomWord/index.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import { StartPage, InteractivePage } from '../../src/component/common';
 import { SelectWord, Setting } from '../../src/component/layout/RandomWord';
+import { useAppDispatch, useAppSelector } from '../../src/redux/hooks';
+import { updateCurrentPage, randomWordSelector } from '../../src/redux/modules/randomWord';
 
 const RandomWord = () => {
+  const dispatch = useAppDispatch();
+  const { currentPage } = useAppSelector(randomWordSelector);
+
+  const handleNextPage = (pageNum: number) => {
+    dispatch(updateCurrentPage(pageNum));
+  };
+
   const pages = [
     {
       component: (
         <StartPage
           title="Six Thinking Hat"
           desc="학교 동아리, 게임 그룹, 세계 예술 감상 커뮤니티에 소속되어 유대감을 느낄 수 있는"
+          onClick={() => handleNextPage(1)}
         />
       ),
     },
     {
-      component: <Setting />,
+      component: <Setting onClick={() => handleNextPage(2)} />,
     },
     {
       component: <SelectWord />,
     },
   ];
 
-  return <InteractivePage pages={pages} currentPage={1} />;
+  return <InteractivePage pages={pages} currentPage={currentPage} />;
 };
 
 export default RandomWord;
--- src/component/layout/RandomWord/Setting.tsx
+++ src/component/layout/RandomWord/Setting.tsx
@@ -3,7 +3,15 @@ import { Button, TextField } from '@mui/material';
 import { HeaderBar, CenterLayout } from '../../common';
 import styled from 'styled-components';
 
-const Setting = () => {
+type SettingProps = {
+  onClick: () => void;
+};
+
+const Setting = ({ onClick }: SettingProps) => {
+  const handleOnClick = () => {
+    if (!onClick) return;
+    onClick();
+  };
   return (
     <>
       <HeaderBar>
@@ -13,12 +21,12 @@ const Setting = () => {
         <SettingWrapper>
           <h1>랜덤워드</h1>
           <p style={{ textAlign: 'center' }}>
-            학교 동아리, 게임 그룹, 세계 예술 감상 커뮤니티에 소속되어 유대감을
-            느낄 수 있는 공간. <br />
+            학교 동아리, 게임 그룹, 세계 예술 감상 커뮤니티에 소속되어 유대감을 느낄 수 있는 공간.{' '}
+            <br />
             소중한 단짝 친구들과 어울릴 수 있는 우리만의 공간.
           </p>
           <TextField />
-          <Button variant="contained">SKIP</Button>
+          <Button variant="contained" onClick={handleOnClick}>SKIP</Button>
         </SettingWrapper>
       </CenterLayout>
     </>
