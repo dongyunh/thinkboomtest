00d1088856fc837da0c3a92db945c9734ed2b493
yechan <noy3928@naver.com>
2022-03-25 15:56:09 +0900

    fix : remove unused code

--- src/components/layout/RandomWord/SelectWord.tsx
+++ src/components/layout/RandomWord/SelectWord.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect } from 'react';
 import { CenterLayout } from '../../common';
-import { useAppDispatch, useAppSelector } from '@redux/hooks';
-import { randomWordSelector, getRandomWord, selectWord } from '@redux/modules/randomWord';
+import { useAppDispatch } from '@redux/hooks';
+import { getRandomWord } from '@redux/modules/randomWord';
 import { SelectWordBox } from '../../layout/RandomWord/SelectWordBox';
 
 const SelectWord = () => {
   const dispatch = useAppDispatch();
-  const { randomWordList, pickedWordList } = useAppSelector(randomWordSelector);
-  console.log(randomWordList);
-  console.log(pickedWordList);
 
   useEffect(() => {
     dispatch(getRandomWord());
